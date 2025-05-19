c8207359f3ab68c37fe5b01be3160d3cff90f70b
Matalentajas <arturorient1305@gmail.com>
2025-05-19 12:38:58 +0200

    Añadir formulario para agregar productos con validaciones y campos necesarios.

--- Crafted3D/app/forms/admin_form.py
+++ Crafted3D/app/forms/admin_form.py
@@ -1,5 +1,5 @@
-from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import DecimalField, EmailField, PasswordField, SelectField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, NumberRange
 from flask_wtf import FlaskForm
 
 # Formulario de Inicio de Sesión para Administrador
@@ -7,3 +7,11 @@ class AdminLoginForm(FlaskForm):
     email = EmailField('Correo Electrónico', validators=[DataRequired()])
     contraseña = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Iniciar Sesión')
+
+class AgregarProductoForm(FlaskForm):
+    nombre_producto = StringField("Nombre del Producto", validators=[DataRequired(), Length(max=100)])
+    descripcion = TextAreaField("Descripción", validators=[DataRequired()])
+    precio = DecimalField("Precio", validators=[DataRequired(), NumberRange(min=0)])
+    imagenes = StringField("URL de la Imagen", validators=[DataRequired(), Length(max=255)])
+    categoria_id = SelectField("Categoría", coerce=int, validators=[DataRequired()])
+    submit = SubmitField("Añadir Producto")
